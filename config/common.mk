f69b6dadf113da8b37ec1aea9160ef0b27d48df0
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-03-20 21:20:11 +0000

    Revert "Add SELinux filesystem relabeling to init"
    This reverts commit d2d1a7ffb1f5e831ed654379496f0efe88159a66.
    
    Revert "selinuxrelabel: Remove attempts to relabel /system"
    
    This reverts commit cae5f245304fe11e86323aa7f6f4c25e898bf36b.
    
    Revert "Recheck /data for selinux labeling"
    
    This reverts commit c4dcb16ca9f51341fdd81a61fb3c00861344c030.
    
    Revert "selinuxrelabel: Add a check for dalvik-cache"
    
    This reverts commit c7e92306143a7b11b570ea110bc467d881a2528d.
    
    Revert "selinuxrelabel: Need to check in priv-app now"
    
    This reverts commit 3013c99acb51f8c2aa1156930e864cfdbcc22135.
    
    Revert "selinux: When adding userdata labels, do cache too"
    
    This reverts commit 8994907baf46dc1ddd78467ec57ff78639c01937.
    
    Revert "selinux: Use a more reliable sentinel to flag /data labelling"
    
    This reverts commit b52874de531340af02ac8e664d828dcd02d2d860.
    
    Change-Id: I7779aaca3b0751ed74d525292681817182a0aa7f
    
    Conflicts:
    	config/common.mk
    	prebuilt/common/etc/init.d/50selinuxrelabel

--- config/common.mk
+++ config/common.mk
@@ -44,10 +44,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
-# SELinux filesystem labels
-PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
-
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
