37cc5fec8a26c00c54561b227453cf72b10160ef
Steve Kondik <steve@cyngn.com>
2015-06-18 02:22:35 -0700

    cm: Update stagefright-plugins depdendencies
    Change-Id: I6be5eea82fc850e837a830de302cd136ad3dc924

--- config/common.mk
+++ config/common.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
     libstagefright_soft_ffmpegadec \
     libstagefright_soft_ffmpegvdec \
     libFFmpegExtractor \
-    libnamparser
+    media_codecs_ffmpeg.xml
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
